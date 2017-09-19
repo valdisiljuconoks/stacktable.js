580e303dd1dd5a5d234212843a734f074fe83755
wałdis iljuczonok <valdis.iljuconoks@outlook.com>
2017-09-19 13:27:10 +0300

    Update stacktable.js

--- stacktable.js
+++ stacktable.js
@@ -160,11 +160,12 @@
       var $table = $(this);
       var $caption = $table.find(">caption").clone();
       var num_cols = $table.find('>thead>tr,>tbody>tr,>tfoot>tr').eq(0).find('>td,>th').length; //first table <tr> must not contain colspans, or add sum(colspan-1) here.
-      if(num_cols<3) //stackcolumns has no effect on tables with less than 3 columns
+      if(typeof settings.column === 'undefined' && num_cols < 3) //stackcolumns has no effect on tables with less than 3 columns
         return;
 
-      var $stackcolumns = $('<table class="stacktable small-only"></table>');
+      var $stackcolumns = $('<table class="stacktable small-only stackcolumns"></table>');
       if (typeof settings.myClass !== 'undefined') $stackcolumns.addClass(settings.myClass);
+      
       $table.addClass('stacktable large-only');
       var tb = $('<tbody></tbody>');
       var col_i = 1; //col index starts at 0 -> start copy at second column.
@@ -173,29 +174,38 @@
         $table.find('>thead>tr,>tbody>tr,>tfoot>tr').each(function(index) {
           var tem = $('<tr></tr>'); // todo opt. copy styles of $this; todo check if parent is thead or tfoot to handle accordingly
           if(index === 0) tem.addClass("st-head-row st-head-row-main");
-          var first = $(this).find('>td,>th').eq(0).clone().addClass("st-key");
-          var target = col_i;
-          // if colspan apply, recompute target for second cell.
-          if ($(this).find("*[colspan]").length) {
-            var i =0;
-            $(this).find('>td,>th').each(function() {
-                var cs = $(this).attr("colspan");
-                if (cs) {
-                  cs = parseInt(cs, 10);
-                  target -= cs-1;
-                  if ((i+cs) > (col_i)) //out of current bounds
-                    target += i + cs - col_i -1;
-                  i += cs;
-                } else {
-                  i++;
-                }
 
-                if (i > col_i)
-                  return false; //target is set; break.
-            });
+          if(typeof settings.columns === 'undefined') {
+            var first = $(this).find('>td,>th').eq(0).clone().addClass("st-key");
+            var target = col_i;
+            // if colspan apply, recompute target for second cell.
+            if ($(this).find("*[colspan]").length) {
+                var i = 0;
+                $(this).find('>td,>th').each(function() {
+                    var cs = $(this).attr("colspan");
+                    if (cs) {
+                    cs = parseInt(cs, 10);
+                    target -= cs-1;
+                    if ((i+cs) > (col_i)) //out of current bounds
+                        target += i + cs - col_i -1;
+                    i += cs;
+                    } else {
+                    i++;
+                    }
+
+                    if (i > col_i)
+                    return false; //target is set; break.
+                });
+            }
+
+            var second = $(this).find('>td,>th').eq(target).clone().addClass("st-val").removeAttr("colspan");
+            tem.append(first, second);
+          } else if(settings.columns === 1) {
+            // collpase all columns into single column
+            var first = $(this).find('>td,>th').eq(col_i - 1).clone().addClass("st-key");
+            tem.append(first);
           }
-          var second = $(this).find('>td,>th').eq(target).clone().addClass("st-val").removeAttr("colspan");
-          tem.append(first, second);
+
           tb.append(tem);
         });
         ++col_i;
